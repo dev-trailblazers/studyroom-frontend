602039cc2cfc24e116a7f7f0ed03add43437bfda
SSY1203 <tlstjsdud566@naver.com>
2024-08-12 16:40:48 +0900

    [styles] Input 컴포넌트 커스텀 완료

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -51,10 +51,10 @@ const Input = ({
       <div
         className={`relative w-full h-[44px] box-border px-[8px] py-[4px] border ${
           isFocused ? 'border border-[#aaaaaa]' : 'border border-[#cccccc]'
-        } rounded-lg bg-white `}
+        } rounded-lg bg-white ${className}`}
       >
         <input
-          className={`w-full p-[10px] box-border border-none bg-transparent outline-none text-[14px] text-gray-900 ${
+          className={`w-full p-[10px] px-[8px] box-border border-none bg-transparent outline-none text-[14px] text-gray-900 ${
             disabled ? 'text-gray-400' : ''
           }`}
           disabled={disabled}
@@ -90,29 +90,6 @@ const Input = ({
       {isError && (
         <span className="text-[11px] text-red-400 px-2">{error}</span>
       )}
-      {/* <div className="relative flex w-full max-w-[24rem]">
-        <CustomInput
-          id={id}
-          data-value={value ? 'true' : 'false'}
-          label={label}
-          value={value}
-          type={isPasswordType && isPasswordVisible ? 'text' : type}
-          disabled={disabled}
-          onChange={onChange}
-          onKeyPress={onKeyPress}
-          onPointerEnterCapture={{}}
-          onPointerLeaveCapture={{}}
-          crossOrigin={{}}
-          containerProps={{
-            className: `${className && `!min-w-0 ${className}`}`,
-          }}
-          className={`bg-white h-[40px] px-[16px] peer relative placeholder-shown:border-gray_DD placeholder-shown:border-t-gray_DD focus:border-gray_DD first:border-t !text-black data-[value=true]:border-gray_DD data-[value=true]:border-t-0 focus:!border focus:!border-t-0  text-[12px]
-          `}
-          labelProps={{
-            className: `peer-placeholder-shown:text-gray_DD peer-placeholder-shown:!text-[12px] peer-placeholder-shown:!pl-1 peer-placeholder-shown:!pt-1 peer-focus:!text-[11px] peer-focus:!pt-0 peer-focus:!pl-0 peer-focus:!text-[#9e9e9e] before:border-gray_DD after:border-gray_DD  peer-focus:before:!border-t peer-focus:before:!border-l peer-focus:before:!border-gray_DD  peer-focus:after:!border-t peer-focus:after:!border-r  peer-focus:after:!border-gray_DD`,
-          }}
-        />
-      </div> */}
     </>
   );
 };
